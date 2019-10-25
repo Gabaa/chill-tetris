a69e4e5beebbbafea12af29dc16b6eec860b4572
Mikkel Gaba <au615597@uni.au.dk>
2019-10-25 13:18:17 +0200

    Added end screen

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def draw_rect(x, y, color):
                                   x * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
                                   (x + 1) * BLOCK_SIZE, (y + 1) * BLOCK_SIZE,
                                   (x + 1) * BLOCK_SIZE, y * BLOCK_SIZE)),
-                         ('c3B', (color * 4)))
+                         ('c3B', color * 4))
 
 
 def draw_gridline(startpoint, endpoint):
@@ -31,7 +31,7 @@ def draw_gridline(startpoint, endpoint):
 
 
 def draw_grid():
-    for x in range(1, COLUMNS):
+    for x in range(1, COLUMNS + 1):
         draw_gridline((x * BLOCK_SIZE, 0), (x * BLOCK_SIZE, BLOCK_SIZE * ROWS))
 
     for y in range(1, ROWS):
@@ -43,63 +43,56 @@ def write_score_to_file(name):
         print(datetime.datetime.now(), name, game.rows_cleared, file=f)
 
 
-class Game:
-    def __init__(self):
-        self.block_factory = BlockFactory()
-        self.block = None
-        self.board = [[None for _ in range(COLUMNS)] for _ in range(ROWS)]
-        self.rows_cleared = 0
+class Block:
+    x: int
+    y: int
 
-        self.set_random_block_as_active()
+    squares: List[Tuple[int, int]]
+    color: Tuple[int, int, int]
 
-    def update(self, dt):
-        if any(self.board[-2]):
-            window.close()
+    def __init__(self, squares, color):
+        self.squares = list(squares)
+        self.color = color
 
-        hit = False
-        for x, y in self.block.squares:
-            if y + self.block.y - 1 < 0 or self.board[y + self.block.y - 1][x + self.block.x]:
-                hit = True
-                break
-        if hit:
-            self.place_block(self.block.color)
-            self.set_random_block_as_active()
+        ylist = [y for x, y in self.squares]
 
-            new_board = []
-            for row in self.board:
-                if not all(row):
-                    new_board.append(row)
+        self.x = COLUMNS // 2 - 1
+        self.y = ROWS - max(ylist) - 1
 
-            while len(new_board) < ROWS:
-                new_board.append([False for _ in range(COLUMNS)])
-                self.rows_cleared += 1
+    def rotate(self, clockwise: bool):
+        # get squares after rotation
+        new_squares = []
 
-            self.board = new_board
-        else:
-            self.block.y -= 1
-
-    def move_block(self, dx):
-        hit = False
-        for x, y in self.block.squares:
-            square_y = self.block.y + y
-            square_x = self.block.x + x + dx
-            if 0 <= square_y < ROWS and 0 <= square_x < COLUMNS:
-                if self.board[self.block.y + y][self.block.x + x + dx]:
-                    hit = True
-                    break
+        for x, y in self.squares:
+            if clockwise:
+                new_squares.append((y, -x))
             else:
-                hit = True
+                new_squares.append((-y, x))
+
+        # check if all squares are now inside
+        all_inside = True
+        for x, y in new_squares:
+            new_x = self.x + x
+            new_y = self.y + y
+            if not (0 <= new_x < COLUMNS and 0 <= new_y < ROWS and not game.board[new_y][new_x]):
+                all_inside = False
+
+        # if they are inside, set as the new block
+        if all_inside:
+            self.squares = new_squares
+
+    def drop(self):
+        will_hit = False
+        while True:
+            for x, y in self.squares:
+                next_y = self.y + y - 1
+                next_x = self.x + x
+                if next_y < 0 or game.board[next_y][next_x]:
+                    will_hit = True
+
+            game.update(0)
+            if will_hit:
                 break
-        if not hit:
-            self.block.x += dx
-
-    def place_block(self, color):
-        for x, y in self.block.squares:
-            if self.block.y + y < len(self.board) and self.block.x + x < len(self.board[self.block.y + y]):
-                self.board[self.block.y + y][self.block.x + x] = color
-
-    def set_random_block_as_active(self):
-        self.block = self.block_factory.create_random_block()
 
 
 class BlockFactory:
@@ -159,67 +152,169 @@ class BlockFactory:
         return Block(random.choice(self.blocks), random.choice(self.colors))
 
 
-class Block:
-    x: int
-    y: int
+class EndScreen:
+    score_label: pyglet.text.Label
+    enter_name_label: pyglet.text.Label
+    name_label: pyglet.text.Label
 
-    squares: List[Tuple[int, int]]
-    color: Tuple[int, int, int]
+    def __init__(self, window, endscore):
+        self.score_label = pyglet.text.Label(f'Score: {endscore}',
+                                             x=window.width // 2,
+                                             y=window.height * 0.7,
+                                             anchor_x='center',
+                                             anchor_y='center',
+                                             font_size=20)
 
-    def __init__(self, squares, color):
-        self.squares = list(squares)
-        self.color = color
+        self.enter_name_label = pyglet.text.Label('ENTER YOUR NAME',
+                                                  x=window.width // 2,
+                                                  y=window.height * 0.8,
+                                                  anchor_x='center',
+                                                  anchor_y='center',
+                                                  font_size=20)
+        self.name_label = pyglet.text.Label('',
+                                            x=window.width // 2,
+                                            y=window.height * 0.5,
+                                            anchor_x='center',
+                                            anchor_y='center',
+                                            font_size=20)
 
-        ylist = [y for x, y in self.squares]
+    def draw(self):
+        self.score_label.draw()
+        self.enter_name_label.draw()
+        self.name_label.draw()
 
-        self.x = COLUMNS // 2 - 1
-        self.y = ROWS - max(ylist) - 1
+    def on_key_press(self, key, mod):
+        if key == pyglet.window.key.BACKSPACE:
+            self.name_label.text = self.name_label.text[:-1]
+        elif key == pyglet.window.key.ENTER:
+            write_score_to_file(self.name_label.text)
+            pyglet.app.exit()
 
-    def rotate(self, clockwise: bool):
-        # get squares after rotation
-        new_squares = []
+    def on_text(self, text):
+        if text.lower() in 'abcdefghijklmnopqrstuvwxyzæøå':
+            self.name_label.text += text
+            print(self.name_label.text)
 
-        for x, y in self.squares:
-            if clockwise:
-                new_squares.append((y, -x))
-            else:
-                new_squares.append((-y, x))
 
-        # check if all squares are now inside
-        all_inside = True
-        for x, y in new_squares:
-            new_x = self.x + x
-            new_y = self.y + y
-            if not (0 <= new_x < COLUMNS and 0 <= new_y < ROWS and not game.board[new_y][new_x]):
-                all_inside = False
+class Game:
+    block_factory: BlockFactory
+    active_block: Block
+    saved_block: Optional[Block]
+    board: List[List[Optional[Tuple[int, int, int]]]]
+    rows_cleared: int
+    ended: bool
+    end_screen: EndScreen
 
-        # if they are inside, set as the new block
-        if all_inside:
-            self.squares = new_squares
+    def __init__(self):
+        self.block_factory = BlockFactory()
+        self.active_block = self.block_factory.create_random_block()
+        self.saved_block = None
+        self.board = [[None for _ in range(COLUMNS)] for _ in range(ROWS)]
+        self.rows_cleared = 0
+        self.ended = False
 
-    def drop(self):
-        will_hit = False
-        while True:
-            for x, y in self.squares:
-                next_y = self.y + y - 1
-                next_x = self.x + x
-                if next_y < 0 or game.board[next_y][next_x]:
-                    will_hit = True
+    def draw(self):
+        if not self.ended:
+            for y, row in enumerate(self.board):
+                for x, color in enumerate(row):
+                    if color is not None:
+                        draw_rect(x, y, color)
 
-            game.update(0)
-            if will_hit:
+            for x, y in self.active_block.squares:
+                draw_rect(x + self.active_block.x, y + self.active_block.y, self.active_block.color)
+
+            if self.saved_block:
+                for x, y in self.saved_block.squares:
+                    draw_rect(BLOCK_SIZE * COLUMNS + x * BLOCK_SIZE + 100, y * BLOCK_SIZE + 100, self.saved_block.color)
+
+            # draw grid
+            draw_grid()
+
+        else:
+            self.end_screen.draw()
+
+    def on_key_press(self, key, mod):
+        if self.ended:
+            self.end_screen.on_key_press(key, mod)
+            return
+
+        if key == pyglet.window.key.LEFT:
+            self.move_block(-1)
+        elif key == pyglet.window.key.RIGHT:
+            self.move_block(1)
+        elif key == pyglet.window.key.UP:
+            self.active_block.rotate(True)
+        elif key == pyglet.window.key.DOWN:
+            self.update(0)
+        elif key == pyglet.window.key.SPACE:
+            self.active_block.drop()
+        elif key == pyglet.window.key.X:
+            self.active_block.rotate(False)
+        elif key == pyglet.window.key.C:
+            self.active_block.rotate(True)
+
+    def on_text(self, text):
+        if self.ended:
+            self.end_screen.on_text(text)
+
+    def update(self, dt):
+        hit = False
+        for x, y in self.active_block.squares:
+            if y + self.active_block.y - 1 < 0 or self.board[y + self.active_block.y - 1][x + self.active_block.x]:
+                hit = True
                 break
+        if hit:
+            self.place_block(self.active_block.color)
+            self.active_block = self.block_factory.create_random_block()
 
+            new_board = []
+            for row in self.board:
+                if not all(row):
+                    new_board.append(row)
 
-player_name = input('Write your name here: ')
+            while len(new_board) < ROWS:
+                new_board.append([None for _ in range(COLUMNS)])
+                self.rows_cleared += 1
+
+            self.board = new_board
+        else:
+            self.active_block.y -= 1
+
+        if any(self.board[-2]):
+            self.end_game()
+
+    def end_game(self):
+        self.ended = True
+        self.end_screen = EndScreen(window, self.rows_cleared)
+        pyglet.clock.unschedule(self.update)
+
+    def move_block(self, dx):
+        hit = False
+        for x, y in self.active_block.squares:
+            square_y = self.active_block.y + y
+            square_x = self.active_block.x + x + dx
+            if 0 <= square_y < ROWS and 0 <= square_x < COLUMNS:
+                if self.board[self.active_block.y + y][self.active_block.x + x + dx]:
+                    hit = True
+                    break
+            else:
+                hit = True
+                break
+        if not hit:
+            self.active_block.x += dx
+
+    def place_block(self, color):
+        for x, y in self.active_block.squares:
+            if self.active_block.y + y < len(self.board) and self.active_block.x + x < len(
+                    self.board[self.active_block.y + y]):
+                self.board[self.active_block.y + y][self.active_block.x + x] = color
+
 
 game = Game()
 
 # Game window
-window = pyglet.window.Window(
-    width=BLOCK_SIZE * COLUMNS,
-    height=BLOCK_SIZE * ROWS
-)
+window = pyglet.window.Window(width=BLOCK_SIZE * COLUMNS + 200,
+                              height=BLOCK_SIZE * ROWS)
 
 pyglet.clock.schedule_interval(game.update, 1 / FPS)
 
@@ -228,37 +323,18 @@ pyglet.clock.schedule_interval(game.update, 1 / FPS)
 @window.event
 def on_draw():
     window.clear()
-    for y, row in enumerate(game.board):
-        for x, color in enumerate(row):
-            if color:
-                draw_rect(x, y, color)
-
-    for x, y in game.block.squares:
-        draw_rect(x + game.block.x, y + game.block.y, game.block.color)
-
-    # draw grid
-    draw_grid()
+    game.draw()
 
 
 @window.event
 def on_key_press(key, mod):
-    if key == pyglet.window.key.LEFT:
-        game.move_block(-1)
-    elif key == pyglet.window.key.RIGHT:
-        game.move_block(1)
-    elif key == pyglet.window.key.UP:
-        game.block.rotate(True)
-    elif key == pyglet.window.key.DOWN:
-        game.update(0)
-    elif key == pyglet.window.key.SPACE:
-        game.block.drop()
-    elif key == pyglet.window.key.X:
-        game.block.rotate(False)
-    elif key == pyglet.window.key.C:
-        game.block.rotate(True)
+    game.on_key_press(key, mod)
+
+
+@window.event
+def on_text(text):
+    game.on_text(text)
 
 
 # Run the game
 pyglet.app.run()
-
-write_score_to_file(player_name)
